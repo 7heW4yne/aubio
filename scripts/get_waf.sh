fb5cea8a5f2e4b446cb4c6f8fa1e80abc1bfec41
7heW4yne <the_wayne@web.de>
2017-08-14 15:45:46 +0200

    Download waf archive and build waf with syms extra

--- scripts/get_waf.sh
+++ scripts/get_waf.sh
@@ -3,8 +3,19 @@
 set -e
 set -x
 
-WAFURL=https://waf.io/waf-1.9.13
+WAFVER=waf-1.9.13
+WAFURL=https://waf.io/waf-1.9.13.tar.bz2
 
-( which wget > /dev/null && wget -qO waf $WAFURL ) || ( which curl > /dev/null && curl $WAFURL > waf )
+mkdir -p waf-tmp
+chmod 755 waf-tmp
+cd waf-tmp > /dev/null 2>&1
+( which wget > /dev/null && wget -qO $WAFVER.tar.bz2 $WAFURL ) || ( which curl > /dev/null && curl -o $WAFVER.tar.bz2 $WAFURL )
+tar jxf ./$WAFVER.tar.bz2 > /dev/null 2>&1
+cd $WAFVER
+python ./waf-light configure build --tools=syms
+cp -fv waf ../../waf > /dev/null 2>&1
+cd ..
+cd ..
+rm -rfv waf-tmp > /dev/null 2>&1
 
 chmod +x waf
